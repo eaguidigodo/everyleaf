14d9ec21c8c7c9390c88bfdee5ee5372eb01b4ef
eaguidigodo <eaguidigodo@gmail.com>
2021-09-29 23:39:58 +0100

    correct error when running rails db:migrate

--- db/migrate/20210928221852_add_contrainst.rb
+++ db/migrate/20210928221852_add_contrainst.rb
@@ -1,6 +1,6 @@
 class AddContrainst < ActiveRecord::Migration[5.2]
   def change
-    change_column :tasks, :title, :string, null: false
+    change_column :tasks, :name, :string, null: false
     change_column :tasks, :detail, :text, null: false
     add_index :tasks, :name, :unique => true
   end
