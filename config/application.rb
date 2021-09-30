a7df71ec44da262afa3fa072422bd2c0f5656a5f
eaguidigodo <eaguidigodo@gmail.com>
2021-09-30 10:42:49 +0100

    internationalisation

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,10 @@ require_relative 'boot'
 
 require 'rails/all'
 
+config.time_zone = 'Africa/Porto-Novo'
+config.active_record.default_timezone = :local
+
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
