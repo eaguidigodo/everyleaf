e5d0b32156a9f53b98884df01d3a1b27c69a6efe
eaguidigodo <eaguidigodo@gmail.com>
2021-10-06 16:57:19 +0100

    added priority feature

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,8 @@
 class Task < ApplicationRecord
     validates :name, :detail, :deadline, presence: true
 
+    enum priority: {  Low: 0, Medium: 1, High: 2 }
+
     scope :search, -> (param) { where("name LIKE ?" , "%#{param}%") }
 
     scope :search_status, -> (param) { where(status: param) }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_05_152551) do
+ActiveRecord::Schema.define(version: 2021_10_06_153216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_10_05_152551) do
     t.datetime "updated_at", null: false
     t.date "deadline", default: "2021-10-01"
     t.text "status"
+    t.integer "priority", default: 0
     t.index ["name"], name: "index_tasks_on_name", unique: true
   end
 
