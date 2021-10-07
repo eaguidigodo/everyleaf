1adb1adf7b439819c317f688d6562415c7ff111e
eaguidigodo <eaguidigodo@gmail.com>
2021-10-07 14:09:14 +0100

    User login working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to tasks_path
+    else
+      flash.now[:danger] = 'I failed to login'
+      render :new
+    end
+
+  end
 end
