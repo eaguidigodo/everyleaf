81501a0c835495b33afdfb928d835e22259f3d43
eaguidigodo <eaguidigodo@gmail.com>
2021-10-11 13:41:05 +0100

    admin function implemente

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -45,7 +45,7 @@ class Admin::UsersController < ApplicationController
       
       respond_to do |format|
         if @user.update(user_params)
-          format.html { redirect_to @user, notice: "Changed user information" }
+          format.html { redirect_to admin_users_path, notice: "Changed user information" }
           format.json { render :show, status: :ok, location: @user }
         else
           format.html { render :edit, status: :unprocessable_entity }
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,14 @@
 FactoryBot.define do
   factory :user do
-    username { "MyString" }
-    email { "MyString" }
-    password { "" }
+    username { "eaguidigodo" }
+    email { "eaguidigodo@gmail.com" }
+    password { "anicetenselme" }
+  end
+
+  factory :second_user, class: User do 
+    username { "wunderkind" }
+    email { "wunderkind@gmail.com" }
+    password { "anicetenselme" }
+    is_admin {"true"}
   end
 end
